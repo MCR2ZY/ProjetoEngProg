8500de894216de08971c896e31f60b77d3effc54
Mauricio Menezes <mauriciosm95@gmail.com>
2019-09-23 13:16:46 -0300

    Alterações feitas em sala.

--- convolutions.py
+++ convolutions.py
@@ -1,123 +1,129 @@
-from skimage.exposure import rescale_intensity
-import numpy as np
 import argparse
-import cv2
 import pdb
 import time
 
-# método de convulsão
-def convolve(image, kernel):
-	# dimensões espaciais da imagem, e do kernel
-	# retorna a altura e largura da imagem em matriz
-	(iH, iW) = image.shape[:2]
-	(kH, kW) = kernel.shape[:2]
+import cv2
+import numpy as np
+from skimage.exposure import rescale_intensity
 
-        # alocar memória para a imagem de saída, tendo o cuidado de
-        # "acoplar" as bordas da imagem de entrada para que o espaço
-        # tamanho (ou seja, largura e altura) não sejam reduzidos
-        # replicando os pixels para que a img de saída tenha o mesmo tamanho
-	pad = (kW - 1) // 2
-	image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
-		cv2.BORDER_REPLICATE)
-	output = np.zeros((iH, iW), dtype="float32")
+i = 1
 
-        # faça um loop sobre a imagem de entrada, "deslizando" o kernel
-        # cada (x, y) - coordena da esquerda para a direita e de cima para
-        # inferior
-	for y in np.arange(pad, iH + pad):
-		for x in np.arange(pad, iW + pad):
-			# extrai a area de interesse da imagem ROI, pegando o
-			# *centro* das coordenadas (x, y)atuais
-			roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+while i > 0:
+	# método de convulsão
+	def convolve(image, kernel):
+		# dimensões espaciais da imagem, e do kernel
+		# retorna a altura e largura da imagem em matriz
+		(iH, iW) = image.shape[:2]
+		(kH, kW) = kernel.shape[:2]
 
-                        #cálculo para convolução
-			k = (roi * kernel).sum()
-			#print(k)
-                        #armazena o valor envolvido na saída (x, y) -
-                        #coordenada da imagem de saída
-			output[y - pad, x - pad] = k
-			 
+			# alocar memória para a imagem de saída, tendo o cuidado de
+			# "acoplar" as bordas da imagem de entrada para que o espaço
+			# tamanho (ou seja, largura e altura) não sejam reduzidos
+			# replicando os pixels para que a img de saída tenha o mesmo tamanho
+		pad = (kW - 1) // 2
+		image = cv2.copyMakeBorder(image, pad, pad, pad, pad,
+			cv2.BORDER_REPLICATE)
+		output = np.zeros((iH, iW), dtype="float32")
 
-        # redimensionar a imagem de saída para estar no intervalo [0, 255]
-	output = rescale_intensity(output, in_range=(0, 255))
-	output = (output * 255).astype("uint8")
+			# faça um loop sobre a imagem de entrada, "deslizando" o kernel
+			# cada (x, y) - coordena da esquerda para a direita e de cima para
+			# inferior
+		for y in np.arange(pad, iH + pad):
+			for x in np.arange(pad, iW + pad):
+				# extrai a area de interesse da imagem ROI, pegando o
+				# *centro* das coordenadas (x, y)atuais
+				roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
 
-	# return a imagem de sáida
-	#print(np.median(output), sep = "\n")
+							#cálculo para convolução
+				k = (roi * kernel).sum()
+				#print(k)
+							#armazena o valor envolvido na saída (x, y) -
+							#coordenada da imagem de saída
+				output[y - pad, x - pad] = k
+				
+
+			# redimensionar a imagem de saída para estar no intervalo [0, 255]
+		output = rescale_intensity(output, in_range=(0, 255))
+		output = (output * 255).astype("uint8")
+
+		# return a imagem de sáida
+		#print(np.median(output), sep = "\n")
+		
+		return output
+
+	# argumento de imagem
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True,
+		help="path to the input image")
+	args = vars(ap.parse_args())
+
+	# kernels
+	gabor = cv2.getGaborKernel((21,21), 5, 1, 10, 1, 0, cv2.CV_32F)
+	mauricio = cv2.getGaborKernel((21,21), 15, 1, 10, 1, 0, cv2.CV_32F)
+	marcelo = cv2.getGaborKernel((21,21), 25, 2, 20, 1, 0, cv2.CV_32F)
+	tamires = cv2.getGaborKernel((21,21), 35, 2, 15, 1, 0, cv2.CV_32F)
+	ricardo = cv2.getGaborKernel((21,21), 8, 1, 4, 3, 0, cv2.CV_32F)
+	gilmar = cv2.getGaborKernel((21,21), 3, 3, 6, 3, 0, cv2.CV_32F)
+
+	# construir o banco de kernel, uma lista de kernels que vamos
+	# aplicar usando a função `convole` e a Função filter2D do OpenCV
+
+	kernelBank = (
+        ("gabor", gabor),
+        ("maricio", mauricio),
+        ("ricardo", ricardo),
+        ("tamires", tamires),
+        ("marcelo", marcelo),
+        ("gilmar", gilmar)        
+	)      
+
+	# carrega a imagem de entrada e convert para escala de cinza.
+	image = cv2.imread(args["image"])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# Kernel Especifico
 	
-	return output
+	for (kernelName, kernel) in kernelBank:
+		
+		line = 0
 
-# argumento de imagem
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
-args = vars(ap.parse_args())
+		# aplicando função convolve
+		tempoInicial = time.time()
 
-# kernels
-smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
-largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
-gabor = cv2.getGaborKernel((21,21), 5, 1, 10, 1, 0, cv2.CV_32F)
+		convoleOutput = convolve(gray, kernel)
 
+		tempoFinal = time.time()
+		tempoExecucao = str(tempoFinal - tempoInicial)
 
-sharpen = np.array((
-	[0, -1, 0],
-	[-1, 5, -1],
-	[0, -1, 0]), dtype="int")
+		linha = line
+		conteudoAux = str(linha) + ' ' + tempoExecucao
 
-laplacian = np.array((
-	[0, 1, 0],
-	[1, -4, 1],
-	[0, 1, 0]), dtype="int")
+		#Criação de Arquivo com os Tempos
+		arquivo = open('tempos.txt', 'r') # Abra o arquivo (leitura)
+		conteudo = arquivo.readlines()
+		if not conteudo:
+			conteudo.append(conteudoAux)
+			arquivo = open('tempos.txt', 'w') # Abre novamente o arquivo (escrita)
+			arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
+			arquivo.close()
+		else:
+			conteudo.append("\n")
+			conteudo.append(conteudoAux)
+			arquivo = open('tempos.txt', 'w') # Abre novamente o arquivo (escrita)
+			arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
+			arquivo.close()
+		line += 1
 
-sobelX = np.array((
-	[-1, 0, 1],
-	[-2, 0, 2],
-	[-1, 0, 1]), dtype="int")
+	# aplicando função2D do openCV
+	opencvOutput = cv2.filter2D(gray, -1, gabor)
 
-sobelY = np.array((
-	[-1, -2, -1],
-	[0, 0, 0],
-	[1, 2, 1]), dtype="int")
+	'''
+	# mostrar imagens de saída
+	cv2.imshow("original", gray)
+	cv2.imshow("{} - convole".format("gabor"), convoleOutput)
 
-# construir o banco de kernel, uma lista de kernels que vamos
-# aplicar usando a função `convole` e a Função filter2D do OpenCV
-
-kernelBank = ("gabor", gabor)      
-
-# carrega a imagem de entrada e convert para escala de cinza.
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Kernel Especifico
-tempoInicial = time.time()
-# aplicando função convolve
-convoleOutput = convolve(gray, gabor)
-
-tempoFinal = time.time()
-tempoExecucao = str(tempoFinal - tempoInicial)
-
-#Criação de Arquivo com os Tempos
-arquivo = open('tempos.txt', 'r') # Abra o arquivo (leitura)
-conteudo = arquivo.readlines()
-if not conteudo:
-	conteudo.append(tempoExecucao)
-	arquivo = open('tempos.txt', 'w') # Abre novamente o arquivo (escrita)
-	arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
-	arquivo.close()
-else:
-	conteudo.append("\n")
-	conteudo.append(tempoExecucao)
-	arquivo = open('tempos.txt', 'w') # Abre novamente o arquivo (escrita)
-	arquivo.writelines(conteudo)    # escreva o conteúdo criado anteriormente nele.
-	arquivo.close()
-
-# aplicando função2D do openCV
-opencvOutput = cv2.filter2D(gray, -1, gabor)
-
-# mostrar imagens de saída
-cv2.imshow("original", gray)
-cv2.imshow("{} - convole".format("gabor"), convoleOutput)
-
-#cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	#cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	'''
+	i-=1
